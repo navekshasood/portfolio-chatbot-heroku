f1b06a8980ac69e3bf5b1c25b9ebd77bcddbeb66
Naveksha Sood <navekshasood@Navekshas-MacBook-Pro.local>
2022-03-25 19:16:33 -0400

    CORS issue solved

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route("/predict", methods=['POST'])
+@cross_origin(origin='*')
 def predict():
     text = request.get_json().get("message")
     response = get_response(text)
